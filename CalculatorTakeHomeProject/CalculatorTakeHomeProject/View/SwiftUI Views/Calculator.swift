a930a0ac7387fac1810527cfa243bce3e1e8cee4
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-01 12:37:06 -0400

    Added Digit Display to Calculator

--- CalculatorTakeHomeProject/CalculatorTakeHomeProject/View/SwiftUI Views/Calculator.swift
+++ CalculatorTakeHomeProject/CalculatorTakeHomeProject/View/SwiftUI Views/Calculator.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct Calculator: View {
+	
+	@EnvironmentObject var viewModel: CalculatorViewModel
+	
     var body: some View {
-        Text("Hello, world!").padding()
+		ZStack {
+			Rectangle()
+				.stroke()
+				.frame(width: 350, height: 540)
+				.overlay(
+					VStack(spacing: 20) {
+						DigitDisplay()
+				}
+			)
+		}
+		.padding(.horizontal)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Calculator()
+			.environmentObject(CalculatorViewModel())
+			.preferredColorScheme(.dark)
     }
 }
