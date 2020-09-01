3348afa5ff096a1495cb51345f86069ffcb6102a
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-01 13:32:49 -0400

    Finished Calculator Project

--- CalculatorTakeHomeProject/CalculatorTakeHomeProject/View/SwiftUI Views/CalculatorButtonsView.swift
+++ CalculatorTakeHomeProject/CalculatorTakeHomeProject/View/SwiftUI Views/CalculatorButtonsView.swift
@@ -12,7 +12,8 @@ struct CalculatorButtonsView: View {
 	let buttons = [["C", "÷"],
 				   ["7", "8", "9", "x"],
 				   ["4", "5", "6", "-"],
-				   ["1", "2", "3", "+"]]
+				   ["1", "2", "3", "+"],
+				   ["0", "="]]
 	
 	@EnvironmentObject var viewModel: CalculatorViewModel
 	@Binding var brain: CalculatorBrain
@@ -25,24 +26,24 @@ struct CalculatorButtonsView: View {
 						CalculatorButton(color: .clearButton, text: digit, width: 240) {
 							viewModel.display = "0"
 						}
+					} else if digit == "0" {
+						CalculatorButton(color: .buttons, text: digit, width: 240) {
+							displayDigits(for: digit)
+						}
 					} else if digit == "7" {
 						CalculatorButton(color: .seven, text: digit, width: 67) {
-							if viewModel.userIsTyping {
-								viewModel.display += digit
-							} else {
-								viewModel.display = digit
-								viewModel.userIsTyping = true
-							}
+							displayDigits(for: digit)
+						}
+					} else if digit == "=" {
+						CalculatorButton(color: .equal, text: digit, width: 67) {
+							performCalculation(with: digit)
 						}
 					} else {
 						CalculatorButton(color: .buttons, text: digit, width: 67) {
 							if Int(digit) == nil {
-								brain.setOperand(viewModel.displayValue)
-								brain.performOperation(digit)
-								if let result = brain.result {
-									viewModel.displayValue = result
-								}
-								viewModel.userIsTyping = false
+								performCalculation(with: digit)
+							} else {
+								displayDigits(for: digit)
 							}
 						}
 					}
@@ -50,6 +51,24 @@ struct CalculatorButtonsView: View {
 			}
 		}
     }
+	
+	func performCalculation(with digit: String) {
+		brain.setOperand(viewModel.displayValue)
+		brain.performOperation(digit)
+		if let result = brain.result {
+			viewModel.displayValue = result
+		}
+		viewModel.userIsTyping = false
+	}
+	
+	func displayDigits(for digit: String) {
+		if viewModel.userIsTyping {
+			viewModel.display += digit
+		} else {
+			viewModel.display = digit
+			viewModel.userIsTyping = true
+		}
+	}
 }
 
 struct CalculatorButtonsView_Previews: PreviewProvider {
