61427b2f4a6be7806f224408e842224000e54d9d
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-01 12:52:20 -0400

    Added Color Extension

---
+++ CalculatorTakeHomeProject/CalculatorTakeHomeProject/Extensions/Color+Ext.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Ext.swift
+//  CalculatorTakeHomeProject
+//
+//  Created by Chad Rutherford on 9/1/20.
+//
+
+import SwiftUI
+
+extension Color {
+	static let equal = Color("EqualColor")
+	static let buttons = Color("MainButtonColor")
+	static let seven = Color("SevenButtonColor")
+	static let clearButton = Color("ClearButtonColor")
+}
