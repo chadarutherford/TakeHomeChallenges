ce64f1483ca64a1b7da3f584b6b56691e49f938a
Chad Rutherford <chad_rutherford@outlook.com>
2020-10-01 10:47:42 -0400

    Added iOS Take Home Challenge

---
+++ JetFuel Take Home/iOS_Take_Home/iOS_Take_Home/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Take_Home
+//
+//  Created by Chad Rutherford on 10/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
